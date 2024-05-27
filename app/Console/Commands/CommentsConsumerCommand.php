56e7c528cd06d9361fd7fa8b3d9e3abc907fd3a6
MatijaBojanic <matija.bojanic@gmail.com>
2024-05-27 20:18:25 +0200

    Subscribing to comment created notifciations

--- app/Console/Commands/CommentsConsumerCommand.php
+++ app/Console/Commands/CommentsConsumerCommand.php
@@ -12,18 +12,58 @@ class CommentsConsumerCommand extends Command
 
     protected $description = 'Command description';
 
-    public function handle(): void
-    {
-        logger('Executed by supervisord');
-        $consumer = Kafka::createConsumer()
-            ->withHandler(function(KafkaConsumerMessage $message) {
-                logger('Caught Comment message');
-                logger($message->getHeaders());
-                logger($message->getBody());
-            })
-            ->subscribe('comments')
-            ->build();
+    private const TOPIC_NAME = 'comments';
+    private const WAIT_TIME = 5;
 
-        $consumer->consume();
+    public function handle()
+    {
+        echo config('kafka.brokers') . PHP_EOL;
+
+        while (true) {
+            $consumer = $this->createConsumer();
+
+            if ($this->tryConsume($consumer)) {
+                break;
+            }
+
+            sleep(self::WAIT_TIME);
+        }
+
+        while (true) {
+            if (!$this->tryConsume($consumer)) {
+                $consumer = $this->createConsumer();
+            }
+
+            sleep(1);
+        }
+    }
+
+    private function createConsumer()
+    {
+        return Kafka::createConsumer()
+            ->withConsumerGroupId('notification-group')
+            ->withHandler(function (KafkaConsumerMessage $message) {
+
+
+            logger($message->getBody());
+
+
+        })->subscribe(self::TOPIC_NAME)->build();
+    }
+
+    private function tryConsume($consumer)
+    {
+        try {
+            $consumer->consume();
+            return true;
+        } catch(\Exception $e) {
+            if ($e->getMessage() !== "Broker: Unknown topic or partition") {
+                logger($e->getMessage());
+                throw $e;
+            }
+
+            logger('Topic does not exist, waiting for it to be created...');
+            return false;
+        }
     }
 }
